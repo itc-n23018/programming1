74ebf23726db6e64878bcd6e02fd3cef473cfd34
Shin Takaesu <n23018@std.it-college.ac.jp>
2023-08-17 06:58:35 +0000

    8/17

---
+++ CHAPTER05/Q5_7_2.py
@@ -0,0 +1,15 @@
+date = (["0001", "Male", "Yamada", "Torou", 25, "Tokyo"],)
+["0002", "Male", "Satou", "Takeshi", 27, "Kanagawa"],
+["0003", "Female", "Tanaka", "Yuko", 25, "Saitama"],
+["0004", "Male", "Suzuki", "Ichirou", 35, "Hokkaido"]
+
+member_information = {}
+
+for record in date:
+    key = record[0]
+    info = record[1:]
+    member_information[key] = info
+
+    print("number", "information", sep="\t")
+    for key, info in member_information.items():
+        print(key, info)
