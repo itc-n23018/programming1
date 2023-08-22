f46477893055da38aff9c8d852e6298961ca3b19
Shin Takaesu <n23018@std.it-college.ac.jp>
2023-08-22 07:24:04 +0000

    8/22

--- CHAPTER04/Q4_8_1.py
+++ CHAPTER04/Q4_8_1.py
@@ -1,10 +1,20 @@
 import time
 
 
-def sleep_for_a_while():
-    """しばらく眠る"""
-    print("Sleeping ..")
-    time.sleep(2)
-    print("Done Sleeping")
+def show_begin_end(func):
+    def deco_func(*args, **kwargs):
+        print("== start")
+        result = func(*args, **kwargs)
+        print("== end")
+        return result
 
-    sleep_for_a_while()
+    return deco_func
+
+
+def sleep_for_a_while():
+    print("眠る")
+    time.sleep(5)
+    print("done sleeping")
+
+
+sleep_for_a_while()
---
+++ CHAPTER07/Q7_2_1.py
@@ -0,0 +1,19 @@
+def list_del_nth(list_, index):
+    try:
+        del list_[index]
+    except IndexError:
+        print("Index Not Found")
+    except TypeError:
+        print("Invalid Index Type")
+    except:
+        print("Unexpected Error")
+    else:
+        print("Successfully")
+
+
+my_list = ["a", "b", "c", "d"]
+list_del_nth(my_list, 5)
+list_del_nth(my_list, 2)
+list_del_nth(my_list, 0)
+
+print(my_list)
