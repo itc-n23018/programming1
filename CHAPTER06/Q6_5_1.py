711d9b3458d561f2b40aa0a9cbd4214eeb54c920
Shin Takaesu <n23018@std.it-college.ac.jp>
2023-09-03 08:35:35 +0000

    today

---
+++ CHAPTER06/Q6_5_1.py
@@ -0,0 +1,13 @@
+import my_math
+
+
+def my_pow(x, y):
+    return x**y
+
+
+with open("my_math.py", "w") as f:
+    f.write(
+        """def my_pow(x, y):
+    return x ** y\n"""
+    )
+print(my_math.my_pow(2, 5))
